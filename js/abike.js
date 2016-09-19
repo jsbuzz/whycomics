2c8ec5c2eb95cf225724e47599a41c6623bf264b
Matyas Buczko <matyas@transferwise.com>
2016-09-19 20:13:20 +0100

    add orphan effect

--- js/abike.js
+++ js/abike.js
@@ -5,39 +5,64 @@ function hideAll() {
 
 $(function() {
 
-  hideAll();
+    hideAll();
 
-  $(document).click(hideAll);
+    $(document).click(hideAll);
 
-  $('.link-box').each(function() {
+    $('.link-box').each(function() {
 
-    $(this) //...
+        $(this) //...
 
-    .mouseenter(debounce(function() {
-        var linkBox = this;
-        //$('img', linkBox).stop().fadeIn();
-        $('.icon, .icon-bg', linkBox).stop().fadeIn();
-        $('a', linkBox).stop().animate({width: '100%'}, function() {
-            $('.description', linkBox).stop().slideDown(100);
-        });
-    }, 800, true))
+        .mouseenter(debounce(function() {
+            var linkBox = this;
+            //$('img', linkBox).stop().fadeIn();
+            $('.icon, .icon-bg', linkBox).stop().fadeIn();
+            $('a', linkBox).stop().animate({width: '100%'}, function() {
+                $('.description', linkBox).stop().slideDown(100);
+            });
+        }, 800, true))
 
-    .mouseleave(function() {
-        var linkBox = this;
-        $('.icon, .icon-bg', linkBox).stop().fadeOut();
-        $('.description', linkBox).stop().slideUp(100, function() {
-            $('a', linkBox).stop().animate({width: '0%'});
+        .mouseleave(function() {
+            var linkBox = this;
+            $('.icon, .icon-bg', linkBox).stop().fadeOut();
+            $('.description', linkBox).stop().slideUp(100, function() {
+                $('a', linkBox).stop().animate({width: '0%'});
+            });
         });
     });
 
-  });
+    $(document).scroll(debounce(function() {
+        $('.icon').fadeIn(200, function() {
+            $('.icon').fadeOut(200);
+        });
+    }, 600))
 
-  $(document).scroll(debounce(function() {
-      $('.icon').fadeIn(200, function() {
-          $('.icon').fadeOut(200);
-      });
-  }, 600))
+    // frame 5
+    $(document).scroll(function() {
+        var leftStart = 0, rightStart = 60.6,
+            scrollStart = $('#comic-strip-5').position().top,
+            scrollTop = document.scrollingElement.scrollTop;
 
+        if(scrollTop > scrollStart && scrollTop < scrollStart + 1000) {
+            var unit = (scrollTop - scrollStart) / 9;
+
+            $('#family-left').css({
+                left : (leftStart-unit)+'%',
+                opacity : (100 - unit*2)/100
+            });
+            $('#family-right').css({
+                left : (rightStart+unit)+'%',
+                opacity : (100 - unit*2)/100
+            });
+        }
+    });
+
+    // cutout background resizing
+    function adjustCutouts() {
+        $('.cutout').css({ backgroundSize : $('.container div:first').width() + 'px' });
+    }
+    $(window).resize(adjustCutouts);
+    adjustCutouts();
 
 });
 

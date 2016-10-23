acb459d9281643fc02bb7af895abb79d80b86a80
Matyas Buczko <matyas@transferwise.com>
2016-10-23 11:45:54 +0100

    fixes

--- js/abike.js
+++ js/abike.js
@@ -16,7 +16,7 @@ $(function() {
         .mouseenter(debounce(function() {
             var linkBox = this;
             //$('img', linkBox).stop().fadeIn();
-            $('.icon, .icon-bg', linkBox).stop().fadeIn();
+            $('.icon, .icon-bg', linkBox).stop().fadeIn().addClass('active');
             $('a', linkBox).stop().animate({width: '100%'}, function() {
                 $('.description', linkBox).stop().slideDown(100);
             });
@@ -24,7 +24,7 @@ $(function() {
 
         .mouseleave(function() {
             var linkBox = this;
-            $('.icon, .icon-bg', linkBox).stop().fadeOut();
+            $('.icon, .icon-bg', linkBox).stop().fadeOut().removeClass('active');
             $('.description', linkBox).stop().slideUp(100, function() {
                 $('a', linkBox).stop().animate({width: '0%'});
             });
@@ -32,12 +32,12 @@ $(function() {
     });
 
     $(document).scroll(debounce(function() {
-        $('.icon').fadeIn(200, function() {
-            $('.icon').fadeOut(200);
+        $('.icon:not(.active)').fadeIn(200, function() {
+            $('.icon:not(.active)').fadeOut(200);
         });
     }, 600))
 
-    $(document).scroll(function() {
+    $(document).scroll(debounce(function() {
         // frame 5
         var leftStart = 0, rightStart = 60.6,
             scrollStart = $('#comic-strip-5').position().top - $('#comic-strip-5').height()/2,
@@ -68,15 +68,15 @@ $(function() {
 
 
         // frame 12
-        scrollStart = $('#comic-strip-12').position().top - $('#comic-strip-12').height()/1.5;
+        scrollStart = $('#comic-strip-12').position().top - $('#comic-strip-12').height()/3;
         if(scrollTop > scrollStart && scrollTop < scrollStart + 3000) {
-            var unit = (scrollTop - scrollStart) / 13;
+            var unit = (scrollTop - scrollStart) / 8;
 
             $('#proposal').css({
                 left: (Math.min(unit - 51, 0))+'%'
             });
         }
-    });
+    }, 10));
 
     // cutout background resizing
     function adjustCutouts() {
